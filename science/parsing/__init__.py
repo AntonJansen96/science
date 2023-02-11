2e4e3917686ef14e925b35d2cf3c499562cc7a6a
Anton Jansen <antonjansen96@gmail.com>
2023-02-11 14:45:32 +0100

    actually put Sanitize in parsing

--- science/parsing/__init__.py
+++ science/parsing/__init__.py
@@ -1,8 +1,9 @@
 """Contains function for parsing data and structure (pdb, gro) files."""
 
-__all__ = ['loadxvg', 'loadCol', 'loadVal', 'Structure']
+__all__ = ['Sanitize', 'loadxvg', 'loadCol', 'loadVal', 'Structure']
 __author__ = 'Anton Jansen'
 
+from .parsing import Sanitize
 from .parsing import loadxvg
 from .parsing import loadCol
 from .parsing import loadVal
