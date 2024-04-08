dead657744b9dd973018c5c477a13c08433bfdf7
Anton Jansen <antonjansen96@gmail.com>
2024-04-08 22:40:53 +0200

    add function for trimming plots using imagemagick

---
+++ bin/trim.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+from science.parsing import Sanitize
+
+# Ensure the user provides a file.
+assert len(sys.argv) == 2, 'Usage: trim.py <file>'
+
+# Get the file path and sanitize it.
+file = Sanitize(sys.argv[1]).path(ext=['.png', '.jpg', '.jpeg'])
+
+# Trim whitespace from an image.
+os.system(f'convert {file} -trim {file}')
