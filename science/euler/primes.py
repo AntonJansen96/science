471384e8079c6e5676fbed27fdbb1b9654eb9b10
Anton Jansen <antonjansen96@gmail.com>
2024-04-26 00:38:28 +0200

    add mersenne prime (exponents)

--- science/euler/primes.py
+++ science/euler/primes.py
@@ -131,6 +131,23 @@ class Primes:
             next += 2
 
     @staticmethod
+    def mersennexponents() -> List[int]:
+        """Returns a list of all known Mersenne prime exponents p.
+        Mersenne primes are prime numbers of the form 2^p - 1 where p is prime.
+
+        Returns:
+            list: Mersenne prime exponents.
+        """
+        # fmt: off
+        return [2, 3, 5, 7, 13, 17, 19, 31, 61, 89, 107, 127, 521, 607, 1279, 
+                2203, 2281, 3217, 4253, 4423, 9689, 9941, 11213, 19937, 21701, 
+                23209, 44497, 86243, 110503, 132049, 216091, 756839, 859433, 
+                1257787, 1398269, 2976221, 3021377, 6972593, 13466917, 20996011, 
+                24036583, 25964951, 30402457, 32582657, 37156667, 42643801, 
+                43112609, 57885161]
+        # fmt:on
+
+    @staticmethod
     def isprime(num: int) -> bool:
         """Check if a number is prime.
 
